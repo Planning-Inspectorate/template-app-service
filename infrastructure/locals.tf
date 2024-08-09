ce7947f773d41ec50f2c936f06112ba1f49ede2b
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-08-09 13:39:48 +0100

    strip everything but some networking out to start project

--- infrastructure/locals.tf
+++ infrastructure/locals.tf
@@ -5,32 +5,31 @@ locals {
 
   resource_suffix = "${local.service_name}-${var.environment}"
 
-  secrets = []
+  # secrets = []
 
-  key_vault_refs = merge(
-    {
-      for k, v in azurerm_key_vault_secret.manual_secrets : k => "@Microsoft.KeyVault(SecretUri=${v.versionless_id})"
-    },
-    {
-      "app-insights-connection-string" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.app_insights_connection_string.versionless_id})",
-      "sql-app-connection-string"      = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.sql_app_connection_string.versionless_id})"
-    }
-  )
+  # key_vault_refs = merge(
+  #   {
+  #     for k, v in azurerm_key_vault_secret.manual_secrets : k => "@Microsoft.KeyVault(SecretUri=${v.versionless_id})"
+  #   },
+  #   {
+  #     "app-insights-connection-string" = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.app_insights_connection_string.versionless_id})",
+  #     "sql-app-connection-string"      = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.sql_app_connection_string.versionless_id})"
+  #   }
+  # )
 
-  common_tags = merge(
-    var.tags,
-    {
+  common_tags = {
       CreatedBy   = "Terraform"
-      ServiceName = local.service_name
+      Environment = var.environment
       location    = local.primary_location
+      Owner       = "DevOps"
+      ServiceName = local.service_name
     }
-  )
 
-  action_group_ids = {
-    tech            = "",
-    service_manager = "",
-    iap             = "",
-    its             = "",
-    info_sec        = ""
-  }
+  # action_group_ids = {
+  #   tech            = "",
+  #   service_manager = "",
+  #   iap             = "",
+  #   its             = "",
+  #   info_sec        = ""
+  # }
 }
