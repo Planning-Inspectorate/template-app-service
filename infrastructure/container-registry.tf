7b4da04038b6621d777ede4f80e3f83eb25d4be2
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-08-14 14:50:39 +0100

    fmt changes

--- infrastructure/container-registry.tf
+++ infrastructure/container-registry.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # resource "azurerm_container_registry" "acr" {
 #   #checkov:skip=CKV_AZURE_137: Admin account required so App services can pull containers when updated by pipeline
 #   #checkov:skip=CKV_AZURE_139: Access over internet required so Azure DevOps can push/pull images
@@ -6,6 +7,16 @@
 #   location            = azurerm_resource_group.primary.location
 #   admin_enabled       = true
 #   sku                 = "Basic"
+=======
+resource "azurerm_container_registry" "acr" {
+  #checkov:skip=CKV_AZURE_137: Admin account required so App services can pull containers when updated by pipeline
+  #checkov:skip=CKV_AZURE_139: Access over internet required so Azure DevOps can push/pull images
+  name                = "pinscr${replace(local.resource_suffix, "-", "")}"
+  resource_group_name = azurerm_resource_group.primary.name
+  location            = azurerm_resource_group.primary.location
+  admin_enabled       = true
+  sku                 = "Basic"
+>>>>>>> afa895c (fmt changes)
 
   tags = local.tags
 }
