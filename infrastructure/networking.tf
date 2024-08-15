052d3857ed2f16f3ea30706c8f6a1e38ab3dd67b
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-08-15 09:28:10 +0100

    database vnet link

--- infrastructure/networking.tf
+++ infrastructure/networking.tf
@@ -91,11 +91,11 @@ data "azurerm_private_dns_zone" "database" {
 
   provider = azurerm.tooling
 }
-# resource "azurerm_private_dns_zone_virtual_network_link" "database" {
-#   name                  = "${local.org}-vnetlink-db-${local.resource_suffix}"
-#   resource_group_name   = var.tooling_config.network_rg
-#   private_dns_zone_name = data.azurerm_private_dns_zone.database.name
-#   virtual_network_id    = azurerm_virtual_network.main.id
+resource "azurerm_private_dns_zone_virtual_network_link" "database" {
+  name                  = "${local.org}-vnetlink-db-${local.resource_suffix}"
+  resource_group_name   = var.tooling_config.network_rg
+  private_dns_zone_name = data.azurerm_private_dns_zone.database.name
+  virtual_network_id    = azurerm_virtual_network.main.id
 
-#   provider = azurerm.tooling
-# }
+  provider = azurerm.tooling
+}
