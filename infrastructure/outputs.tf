f38b335906770483605945815bd9855747cac80d
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-10-21 15:21:08 +0100

    test: updating output module name

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,10 +1,10 @@
-output "template_secondary_mapping-url" {
+output "template_secondary_mapping_url" {
   description = "The URL of the web frontend App Service"
   value       = module.template_app_web.default_site_hostname
 }
 
 
-output "template_primary_mapping-url" {
+output "template_primary_mapping_url" {
   description = "The URL of the web frontend App Service"
   value       = module.template_app_web.default_site_hostname
 }
