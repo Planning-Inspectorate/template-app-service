96dfaf1309b86980419abb5de9cbb8477dd6014e
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-10-21 13:56:54 +0100

    test: updating backend address url

--- infrastructure/front-door-web.tf
+++ infrastructure/front-door-web.tf
@@ -42,7 +42,7 @@ resource "azurerm_frontdoor" "common" {
 
       content {
         enabled     = true
-        address     = var.web_app_domain
+        address     = app_service_url.value["url"]
         host_header = local.template_frontend.infer_backend_host_header ? "" : app_service_url.value["url"]
         http_port   = 80
         https_port  = 443
@@ -66,6 +66,8 @@ resource "azurerm_frontdoor" "common" {
       forwarding_protocol    = "MatchRequest"
     }
   }
+
+  tags = local.tags
 
 
 }
