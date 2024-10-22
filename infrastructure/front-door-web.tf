f67c0cf3f65316af0f7f57b48942b4ec1dd72a43
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-10-22 09:24:34 +0100

    test: adding domin and rputing rule for template

--- infrastructure/front-door-web.tf
+++ infrastructure/front-door-web.tf
@@ -25,6 +25,7 @@ resource "azurerm_frontdoor" "common" {
     backend_pools_send_receive_timeout_seconds   = 120
   }
 
+  #default with froontdoor name
   frontend_endpoint {
     name                                    = "pins-fd-${local.service_name}-${local.resource_suffix}"
     host_name                               = "pins-fd-${local.service_name}-${local.resource_suffix}.azurefd.net"
@@ -54,9 +55,9 @@ resource "azurerm_frontdoor" "common" {
 
   routing_rule {
     enabled            = true
-    name               = local.template_frontend.name
+    name               = "Default"
     accepted_protocols = ["Http", "Https"]
-    patterns_to_match  = local.template_frontend.patterns_to_match
+    patterns_to_match  = ["/*"]
     frontend_endpoints = ["pins-fd-${local.service_name}-${local.resource_suffix}"]
 
     forwarding_configuration {
@@ -67,8 +68,48 @@ resource "azurerm_frontdoor" "common" {
     }
   }
 
-  tags = local.tags
+  #For template host
+  frontend_endpoint {
+    name                                    = local.template_frontend.frontend_name
+    host_name                               = local.template_frontend.frontend_endpoint
+    web_application_firewall_policy_link_id = azurerm_frontdoor_firewall_policy.template_frontend.id
+  }
+  # backend_pool {
+  #   name                = local.template_frontend.frontend_name
+  #   load_balancing_name = "Default"
+  #   health_probe_name   = "Http"
 
+  #   dynamic "backend" {
+  #     for_each = local.template_frontend.app_service_urls
+  #     iterator = app_service_url
+
+  #     content {
+  #       enabled     = true
+  #       address     = app_service_url.value["url"]
+  #       host_header = local.template_frontend.infer_backend_host_header ? "" : app_service_url.value["url"]
+  #       http_port   = 80
+  #       https_port  = 443
+  #       priority    = app_service_url.value["priority"]
+  #       weight      = 100
+  #     }
+  #   }
+  # }
+  routing_rule {
+    enabled            = true
+    name               = local.template_frontend.name
+    accepted_protocols = ["Http", "Https"]
+    patterns_to_match  = local.template_frontend.patterns_to_match
+    frontend_endpoints = [local.template_frontend.frontend_name]
+
+    forwarding_configuration {
+      backend_pool_name      = local.template_frontend.name
+      cache_enabled          = false
+      cache_query_parameters = []
+      forwarding_protocol    = "MatchRequest"
+    }
+  }
+
+  tags = local.tags
 
 }
 # --------------------------------FRONT DOOR STANDARD CODE BELOW---------------------------------------------------------------
