7ca14720dba1647cd6fbb6be57a48e954768ae89
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2024-11-07 10:34:16 +0000

    # This is a combination of 5 commits. # This is the 1st commit message:
    upgrading to azreurm to version 4 to match modules
    
    # The commit message #2 will be skipped:
    
    # upgrading to azreurm to version 4 to match modules
    
    # The commit message #3 will be skipped:
    
    # upgrading to azreurm to version 4 to match modules
    
    # The commit message #4 will be skipped:
    
    # upgrading to azreurm to version 4 to match modules
    
    # The commit message #5 will be skipped:
    
    # upgrading to azreurm to version 4 to match modules

--- infrastructure/app-web.tf
+++ infrastructure/app-web.tf
@@ -1,6 +1,6 @@
 module "template_app_web" {
   #checkov:skip=CKV_TF_1: Use of commit hash are not required for our Terraform modules
-  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=0a1314b2a7668de323fcad6589455d2f7f60d8be"
+  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=c38626f"
 
   resource_group_name = azurerm_resource_group.primary.name
   location            = module.primary_region.location
