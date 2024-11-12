60b7c9384bf48661e8df3e6213db025e16725b62
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-11-12 16:05:07 +0000

    adding provider

--- infrastructure/front-door-premium.tf
+++ infrastructure/front-door-premium.tf
@@ -11,7 +11,7 @@
 resource "azurerm_cdn_frontdoor_origin_group" "web" {
   name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
-
+  provider                 = azurerm.tooling
   session_affinity_enabled = true
 
   health_probe {
@@ -29,10 +29,10 @@ resource "azurerm_cdn_frontdoor_origin_group" "web" {
 }
 
 resource "azurerm_cdn_frontdoor_origin" "web" {
-  name                          = "${local.org}-fd-${local.service_name}-web-${var.environment}"
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
-  enabled                       = true
-
+  name                           = "${local.org}-fd-${local.service_name}-web-${var.environment}"
+  cdn_frontdoor_origin_group_id  = azurerm_cdn_frontdoor_origin_group.web.id
+  enabled                        = true
+  provider                       = azurerm.tooling
   certificate_name_check_enabled = true
 
   host_name          = module.template_app_web.default_site_hostname
@@ -54,6 +54,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "web" {
   name                     = "${local.org}-fd-${local.service_name}-web-${var.environment}"
   cdn_frontdoor_profile_id = data.azurerm_cdn_frontdoor_profile.web.id
   host_name                = var.web_app_domain
+  provider                 = azurerm.tooling
 
   tls {
     certificate_type    = "ManagedCertificate"
@@ -66,6 +67,7 @@ resource "azurerm_cdn_frontdoor_route" "web" {
   cdn_frontdoor_endpoint_id     = data.azurerm_cdn_frontdoor_endpoint.web.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.web.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.web.id]
+  provider                      = azurerm.tooling
 
   forwarding_protocol    = "MatchRequest"
   https_redirect_enabled = true
@@ -80,6 +82,7 @@ resource "azurerm_cdn_frontdoor_route" "web" {
 resource "azurerm_cdn_frontdoor_custom_domain_association" "web" {
   cdn_frontdoor_custom_domain_id = azurerm_cdn_frontdoor_custom_domain.web.id
   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.web.id]
+  provider                       = azurerm.tooling
 }
 
 # WAF policy
