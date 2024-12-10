27f7f7e92a5b0da185a253f410148ed5feb6cecd
Medini-Aithal <medini.aithal.zc@planninginspectorate.gov.uk>
2024-12-10 16:08:46 +0000

    making auth chnages to default slot

--- infrastructure/app-web.tf
+++ infrastructure/app-web.tf
@@ -1,6 +1,6 @@
 module "template_app_web" {
   #checkov:skip=CKV_TF_1: Use of commit hash are not required for our Terraform modules
-  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=3b1ef293e3d1d9ce34353f845283aa3c6b1b9f9b"
+  source = "github.com/Planning-Inspectorate/infrastructure-modules.git//modules/node-app-service?ref=8471932f7fc14aaf630ec9424a41ff959cbf7839"
 
   resource_group_name = azurerm_resource_group.primary.name
   location            = module.primary_region.location
