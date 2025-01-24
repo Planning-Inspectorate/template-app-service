8eb6169826c17ba21a8c4a2ade2a4295dd693cc5
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-01-24 09:33:45 +0000

    feat(app): add health endpoint

--- app/src/app/routes/router.js
+++ app/src/app/routes/router.js
@@ -1,11 +1,19 @@
 import { Router as createRouter } from 'express';
 
 import { viewHomepage } from '../pages/home/home.controller.js';
+import {createMonitoringRoutes} from "./monitoring.js";
+import logger from "../../util/logger.js";
+import config from "../config.js";
 
 // create an express router, for handling different paths
 const router = createRouter();
+const monitoringRoutes = createMonitoringRoutes({
+    config,
+    logger,
+});
 
 // setup the handlers for the pages
+router.use('/', monitoringRoutes);
 router.route('/').get(viewHomepage);
 
 // export the router for use by the app
