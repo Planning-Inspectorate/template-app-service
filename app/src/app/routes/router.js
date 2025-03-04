eef4d1929116aa54d542d0b6c80364273847f06f
Ben Jacobs <benjamin.jacobs@planninginspectorate.gov.uk>
2025-03-04 09:28:43 +0000

    feat(app): add example POST endpoint

--- app/src/app/routes/router.js
+++ app/src/app/routes/router.js
@@ -4,6 +4,7 @@ import { viewHomepage } from '../pages/home/home.controller.js';
 import {createMonitoringRoutes} from "./monitoring.js";
 import logger from "../../util/logger.js";
 import config from "../config.js";
+import {handlePost} from "../pages/post/post.controller.js";
 
 // create an express router, for handling different paths
 const router = createRouter();
@@ -15,6 +16,7 @@ const monitoringRoutes = createMonitoringRoutes({
 // setup the handlers for the pages
 router.use('/', monitoringRoutes);
 router.route('/').get(viewHomepage);
+router.route('/post').post(handlePost);
 
 // export the router for use by the app
 export default router;
