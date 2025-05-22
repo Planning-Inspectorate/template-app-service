a19a85d5624d021f48da3b3c69134a2611b5cb72
m-juckes-pins <michael.juckes@planninginspectorate.gov.uk>
2025-05-22 14:21:31 +0100

    ci: create-new-test-agent-pipeline-checkov-v3

--- packer/image-build/tools-odw.sh
+++ packer/image-build/tools-odw.sh
@@ -1,5 +1,6 @@
 export DEBIAN_FRONTEND=noninteractive
 
+sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo echo 'APT::Acquire::Retries "3";' > /etc/apt/apt.conf.d/80-retries
 sudo echo "APT::Get::Assume-Yes \"true\";" > /etc/apt/apt.conf.d/90assumeyes
 
@@ -7,14 +8,13 @@ sudo add-apt-repository main
 sudo add-apt-repository restricted
 sudo add-apt-repository universe
 sudo add-apt-repository multiverse
-sudo apt update
+sudo add-apt-repository ppa:git-core/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa
 
 sudo apt-get clean && apt-get update && apt-get upgrade
 sudo apt-get install -y --no-install-recommends \
-  apt-transport-https \
   build-essential \
   ca-certificates \
-  unixodbc-dev \
   curl \
   gnupg \
   jq \
@@ -30,42 +30,89 @@ sudo apt-get install -y --no-install-recommends \
   lsb-release \
   software-properties-common \
   unzip \
-  wget \
   xauth \
   xvfb \
   zip
 
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:deadsnakes/ppa
-
 # Git
-sudo apt install -y --no-install-recommends \
+sudo apt-get install -y --no-install-recommends \
   git \
   git-lfs \
   git-ftp
 
-echo "==================== PYTHON DEFAULT VERSION ===================="
-python3 --version
-echo "================================================================"
+# Python
+sudo apt-get install -y --no-install-recommends \
+  python3.7 \
+  python3.7-distutils \
+  python3-pip
+
+# Docker Engine
+sudo apt-get install -y docker.io
+
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Powershell
+sudo snap install powershell --classic
+
+# Terraform 1.11.4
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+sudo apt-get install -y terraform=1.11.4-1 # the hyphen is needed for the repo
+
+# Terragrunt 0.55.1
+sudo curl -s -L "https://github.com/gruntwork-io/terragrunt/releases/download/v0.55.1/terragrunt_linux_amd64" -o /usr/bin/terragrunt && chmod 777 /usr/bin/terragrunt
+
+# Checkov
+python3.7 -m pip install --force-reinstall packaging==21
+python3.7 -m pip install -U checkov==3.2.405
+
+# TFLint
+curl -s https://raw.githubusercontent.com/terraform-linters/tflint/master/install_linux.sh | bash
+
+# NVM
+sudo mkdir /usr/local/nvm && chmod -R 777 /usr/local/nvm
+sudo curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | NVM_DIR=/usr/local/nvm bash
+
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export PATH="$PATH:$NVM_DIR"
+
+sudo tee /etc/skel/.bashrc > /dev/null <<"EOT"
+export NVM_DIR="/usr/local/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+export PATH="$PATH:$NVM_DIR"
+EOT
+
+# Node versions
+nvm install 22
+nvm install 20
+nvm install 18
+nvm install 16
+nvm install 15
+nvm install 14
+
+nvm alias default 20
+nvm use default
 
 # Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
 
-# Azure Tools
-sudo curl -fsSL https://aka.ms/install-azd.sh | bash
-
-# .NET Core and PowerShell
+# .NET Core
 wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 
 sudo apt-get update; \
-  sudo apt-get install -y aspnetcore-runtime-6.0 && \
-  sudo apt-get install -y powershell
+  sudo apt-get install -y apt-transport-https && \
+  sudo apt-get update && \
+  sudo apt-get install -y aspnetcore-runtime-6.0
 
-# PowerShell Modules
-pwsh -c "& {Install-Module -Name Az -Scope AllUsers -Repository PSGallery -Force -Verbose}"
-pwsh -c "& {Get-Module -ListAvailable}"
-
-# Sysprep
 /usr/sbin/waagent -force -deprovision+user && export HISTSIZE=0 && sync
